6e04314148ad346cbd56ea305a888bff2da3010e
ashikbin123 <165547480+ashikbin123@users.noreply.github.com>
2024-04-28 19:34:35 +0600

    Update antiout.js

--- scripts/events/antiout.js
+++ scripts/events/antiout.js
@@ -16,7 +16,7 @@ module.exports.run = async({ event, api, Threads, Users }) => {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
     api.sendMessage(`Unable to re-add members ${name} to the group\n\n${name} blocked me or There is no Message option in the profile `, event.threadID)
-   } else api.sendMessage(`${name} বদমাইশ তুমি কোনো বাহানা করতে পারব না আশিক বস এর অর্ডার `, event.threadID);
+   } else api.sendMessage(`${name} বদমাইশ তুই কোনো বাহানা করতে পারবি না আশিক বস এর অর্ডার চুপ করে গ্রুপে থাক না হলে তোরে মারবে আশিক বস`, event.threadID);
   })
  }
 }
